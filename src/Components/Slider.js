ce22ca4d0ca06c32d62f8fc29c2bc973b572c404
Furqan <furqanhashmi@gmail.com>
2024-06-25 19:05:09 +0500

    made changes to slide file

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -12,21 +12,21 @@ const Slider = () => {
     <ol className="breadcrumb breadcrumb-custom overflow-hidden text-center bg-body-tertiary border rounded-3 firstfootermid">
       <li className='unlimited'>
         <a className="link-body-emphasis fw-semibold text-decoration-none" href="#">
-          <i><img src='/images/01.png' alt='img'/></i>
+          <i><img src='/Images/img 1.png' alt='img'/></i>
           <h4>Unlimited Access</h4>
           <p>Choose What you’d like to learn from our extensive <br></br>subscription library.</p>
         </a>
       </li>
       <li className='learn'>
         <a className="link-body-emphasis fw-semibold text-decoration-none" href="#">
-          <i><img src='/images/02.png' alt='img'/></i>
+          <i><img src='/Images/img 2.png' alt='img'/></i>
           <h4>Learn Anywhere</h4>
           <p>Learn from industry experts who are passionate <br></br>about teaching.</p>
         </a>
       </li>
       <li className='customize'>
         <a className="link-body-emphasis fw-semibold text-decoration-none" href="#">
-          <i><img src='/images/03.png' alt='img'/></i>
+          <i><img src='/Images/img 3.png' alt='img'/></i>
           <h4>Customizability</h4>
           <p>Switch between your computer, tablet, or mobile <br></br>device.</p>
         </a>
